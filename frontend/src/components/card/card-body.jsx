ab1a60f65d2e10c176d6458868dba5c90148d350
dewolfe <>
2024-06-25 14:58:22 -0700

    fix: card scales down correctly with aspect ratio

--- frontend/src/components/card/card-body.jsx
+++ frontend/src/components/card/card-body.jsx
@@ -3,13 +3,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './card.css'; // Import a CSS file for styling (optional)
 
-const CardBody = ({ title, content, imageUrl }) => {
+const CardBody = ({ title, content, imageUrl, minHeight, minWidth, aspectRatio }) => {
+  const cardBodyStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    border: "1px solid #ddd",
+    borderRadius: "5px",
+    padding: "20px",
+    margin: "20px",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+    maxWidth: "100%",
+    maxHeight: "100%",
+    minHeight: minHeight ? `${minHeight}px` : "auto",
+    minWidth: minWidth ? `${minWidth}px` : "100%",
+    aspectRatio: aspectRatio ? `${aspectRatio}` : "5/7"
+  };
+
   return (
-    <div className="card-body">
-      {imageUrl && <img src={imageUrl} alt={title} className="card-img-top" />}
+    <div style={cardBodyStyle}>
+      <img src={imageUrl} alt={title} className="card-img-top" />
       <div className="card-content">
-        {title && <h5 className="card-title">{title}</h5>}
-        {content && <p className="card-text">{content}</p>}
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{content}</p>
       </div>
     </div>
   );
@@ -19,12 +35,18 @@ CardBody.propTypes = {
   title: PropTypes.string,
   content: PropTypes.string,
   imageUrl: PropTypes.string,
+  height: PropTypes.number,
+  width: PropTypes.number,
+  aspectRatio: PropTypes.string
 };
 
 CardBody.defaultProps = {
   title: '',
   content: '',
   imageUrl: '',
+  height: undefined,
+  width: undefined,
+  aspectRatio: undefined
 };
 
 export default CardBody;
--- frontend/src/components/card/card.jsx
+++ frontend/src/components/card/card.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ReactCardFlip from 'react-card-flip';
 import Rnd from 'react-rnd-rotate-restore';
 import CardBody from './card-body';
@@ -10,8 +10,19 @@ const Card  = (props) => {
     const [disableDragging, setDisableDragging] = useState(false);
     const [x, setX] = useState(0);
     const [y, setY] = useState(0);
-    const [width, setWidth] = useState(320);
-    const [height, setHeight] = useState(200);
+    const [width, setWidth] = useState(200);
+    const [height, setHeight] = useState(320);
+
+    const [minWidth, setMinWidth] = useState(0);
+    const [minHeight, setMinHeight] = useState(0);
+
+    const [cardMinWidth, setCardMinWidth] = useState(200);
+    const [cardMinHeight, setCardMinHeight] = useState(320);
+
+    const cardRef = useRef(undefined);
+
+    const frontCardRef = useRef(undefined);
+    const backCardRef = useRef(undefined);
 
     const HandleFlip = () => {
         setIsFlipped(prevState => !prevState);
@@ -26,70 +37,92 @@ const Card  = (props) => {
         { name: 'Draggable?', value: '2', onClick: HandleDisableDragging}
       ];
 
-      const RndStyle = {
+    const RndStyle = {
         display: "flex",
         border: "solid 1px #ddd",
         background: "#f0f0f0",
     };
 
+    // So this is here to update every time the resize happens to make sure the container's min width and height updates with it
+    const SetCardContainerMinSize = () => {
+        if (cardRef.current) {
+            const cardRefWidth = cardRef.current.getBoundingClientRect().width;
+            const cardRefHeight = cardRef.current.getBoundingClientRect().height;
+
+            setMinWidth(cardRefWidth);
+            setMinHeight(cardRefHeight);
+
+            console.log(`Container dimensions: ${width}px x ${height}px\nCard dimensions: ${cardRefWidth}px x ${cardRefHeight }px`);
+
+            return true;
+        }
+
+        return false;
+    };
+
+    const CalculateCardBodyAspectRatio = (cardBodyWidth, cardBodyHeight) => {
+        return `${cardBodyWidth}/${cardBodyHeight}`;
+    }
+
+    /*const setCardContainerMinSizeEffect = useEffect(() => {
+        if (cardRef.current) {
+            setMinWidth(cardRef.current.getBoundingClientRect().width);
+            setMinHeight(cardRef.current.getBoundingClientRect().height);
+        }
+      }, [x, y]);*/
+
     return (
         <Rnd
             style = {RndStyle}
             position={{ x: x, y: y }}
+            minHeight = {minHeight}
+            minWidth = {minWidth}
+            lockAspectRatio = {true}
             onDragStop={(e, d) => { setX(d.x); setY(d.y); }}
             disableDragging = {disableDragging}
             onResize={(e, direction, ref, delta, position) => {
-                // setWidth(ref.offsetWidth);
-                // setHeight(ref.offsetHeight);
+                setWidth(ref.offsetWidth);
+                setHeight(ref.offsetHeight);
                 setX(position.x);
                 setY(position.y);
+
+                SetCardContainerMinSize();
             }}
         >
-            <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-                <div>
+            <div ref = {cardRef}>
+                <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
                     <CardBody 
                         title="Front"
                         content="This is some example content for the card."
                         imageUrl="https://via.placeholder.com/150"
+                        minHeight={cardMinHeight}
+                        minWidth={cardMinWidth}
+                        aspectRatio= {CalculateCardBodyAspectRatio(466.72, 591.72)}
                     />
-                    <ButtonGroup className="mb-2">
-                        {radios.map((radio, idx) => (
-                        <ToggleButton
-                            key={idx}
-                            id={`radio-${idx}`}
-                            type="radio"
-                            variant="secondary"
-                            name="radio"
-                            value={radio.value}
-                            onClick ={radio.onClick}
-                        >
-                            {radio.name}
-                        </ToggleButton>
-                        ))}
-                    </ButtonGroup>
-                </div>
-                <div>
                     <CardBody 
                         title="Back"
                         content="Another piece of content."
+                        minHeight={cardMinHeight}
+                        minWidth={cardMinWidth}
+                        aspectRatio= {CalculateCardBodyAspectRatio(466.72, 591.72)}
                     />
-                    <ButtonGroup className="mb-2">
-                        {radios.map((radio, idx) => (
-                        <ToggleButton
-                            key={idx}
-                            id={`radio-${idx}`}
-                            type="radio"
-                            variant="secondary"
-                            name="radio"
-                            value={radio.value}
-                            onClick ={radio.onClick}
-                        >
-                            {radio.name}
-                        </ToggleButton>
-                        ))}
-                    </ButtonGroup>
-                </div>
-            </ReactCardFlip>
+                </ReactCardFlip>
+                <ButtonGroup className="mb-2">
+                            {radios.map((radio, idx) => (
+                            <ToggleButton
+                                key={idx}
+                                id={`radio-${idx}`}
+                                type="radio"
+                                variant="secondary"
+                                name="radio"
+                                value={radio.value}
+                                onClick ={radio.onClick}
+                            >
+                                {radio.name}
+                            </ToggleButton>
+                            ))}
+                </ButtonGroup>
+            </div>
         </Rnd>
       )
 }
