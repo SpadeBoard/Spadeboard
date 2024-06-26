afc93281fbcc5980318be4523d745071ba570a31
dewolfe <>
2024-06-26 09:06:14 -0700

    refactor: split card into card and component-container to have rnd functionality be reusable

--- frontend/src/components/card/card.jsx
+++ frontend/src/components/card/card.jsx
@@ -1,95 +1,29 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import ReactCardFlip from 'react-card-flip';
-import Rnd from 'react-rnd-rotate-restore';
 import CardBody from './card-body';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 
 const Card  = (props) => {
     const [isFlipped, setIsFlipped] = useState(false);
-    const [disableDragging, setDisableDragging] = useState(false);
-    const [x, setX] = useState(0);
-    const [y, setY] = useState(0);
-    const [width, setWidth] = useState(200);
-    const [height, setHeight] = useState(320);
-
-    const [minWidth, setMinWidth] = useState(0);
-    const [minHeight, setMinHeight] = useState(0);
-
+   
     const [cardMinWidth, setCardMinWidth] = useState(200);
     const [cardMinHeight, setCardMinHeight] = useState(320);
-
-    const cardRef = useRef(undefined);
-
-    const frontCardRef = useRef(undefined);
-    const backCardRef = useRef(undefined);
 
     const HandleFlip = () => {
         setIsFlipped(prevState => !prevState);
     };
 
-    const HandleDisableDragging = () => {
-        setDisableDragging(prevState => !prevState);
-    };
-    
     const radios = [
-        { name: 'Flip', value: '1', onClick: HandleFlip},
-        { name: 'Draggable?', value: '2', onClick: HandleDisableDragging}
+        { name: 'Flip', value: '1', onClick: HandleFlip}
       ];
-
-    const RndStyle = {
-        display: "flex",
-        border: "solid 1px #ddd",
-        background: "#f0f0f0",
-    };
-
-    // So this is here to update every time the resize happens to make sure the container's min width and height updates with it
-    const SetCardContainerMinSize = () => {
-        if (cardRef.current) {
-            const cardRefWidth = cardRef.current.getBoundingClientRect().width;
-            const cardRefHeight = cardRef.current.getBoundingClientRect().height;
-
-            setMinWidth(cardRefWidth);
-            setMinHeight(cardRefHeight);
-
-            console.log(`Container dimensions: ${width}px x ${height}px\nCard dimensions: ${cardRefWidth}px x ${cardRefHeight }px`);
-
-            return true;
-        }
-
-        return false;
-    };
 
     const CalculateCardBodyAspectRatio = (cardBodyWidth, cardBodyHeight) => {
         return `${cardBodyWidth}/${cardBodyHeight}`;
     }
 
-    /*const setCardContainerMinSizeEffect = useEffect(() => {
-        if (cardRef.current) {
-            setMinWidth(cardRef.current.getBoundingClientRect().width);
-            setMinHeight(cardRef.current.getBoundingClientRect().height);
-        }
-      }, [x, y]);*/
-
     return (
-        <Rnd
-            style = {RndStyle}
-            position={{ x: x, y: y }}
-            minHeight = {minHeight}
-            minWidth = {minWidth}
-            lockAspectRatio = {true}
-            onDragStop={(e, d) => { setX(d.x); setY(d.y); }}
-            disableDragging = {disableDragging}
-            onResize={(e, direction, ref, delta, position) => {
-                setWidth(ref.offsetWidth);
-                setHeight(ref.offsetHeight);
-                setX(position.x);
-                setY(position.y);
-
-                SetCardContainerMinSize();
-            }}
-        >
-            <div ref = {cardRef}>
+        <div>
                 <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
                     <CardBody 
                         title="Front"
@@ -123,7 +57,6 @@ const Card  = (props) => {
                             ))}
                 </ButtonGroup>
             </div>
-        </Rnd>
       )
 }
 
--- frontend/src/pages/game-room/game-room.jsx
+++ frontend/src/pages/game-room/game-room.jsx
@@ -1,7 +1,12 @@
+import ComponentContainer from '../../utils/component-container';
 import Card from '../../components/card/card';
 
 const GameRoom = (props) => {
-    return (<Card/>)
+    return (
+        <ComponentContainer>
+          <Card />
+        </ComponentContainer>
+      );
 }
 
 export default GameRoom;
