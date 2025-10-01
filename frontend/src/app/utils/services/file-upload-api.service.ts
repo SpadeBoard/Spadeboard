cd9ddf03c2019b1928c7657144df6f5e17a53b85
dewolfe <>
2025-10-01 10:24:37 -0700

    feat: added a few CRUD batch operations for file uploading in the frontend

--- frontend/src/app/utils/services/file-upload-api.service.ts
+++ frontend/src/app/utils/services/file-upload-api.service.ts
@@ -1,7 +1,6 @@
-import { inject, Injectable, ResourceRef } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { rxResource } from '@angular/core/rxjs-interop';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -9,19 +8,32 @@ import { Observable } from 'rxjs';
 })
 export class FileUploadApiService {
   private http = inject(HttpClient);
-  
+
   private apiUrl = `${environment.hostServerUrl}/api/Files`;
 
   constructor() { }
 
+  // TODO: Revamp this system, the switch statements are kind of ridiculous, I'm sorry
+
+  // TODO: Finish this
+
+  // https://stackoverflow.com/questions/62539753/extract-zip-file-and-read-the-data-inside-the-file-in-angular
+  getFiles$(fileNames: string[], type?: string): Observable<Blob | undefined> {
+    switch (type) {
+      case "card-face":
+        return this.http.get(`${this.apiUrl}/card-face/lods/${fileNames}`, { responseType: 'blob' });
+      default:
+        return this.http.get(`${this.apiUrl}/${fileNames}`, { responseType: 'blob' });
+    }
+  }
+
   getFile$(fileName: string, type?: string): Observable<Blob | undefined> {
-    switch (type)
-    {
+    switch (type) {
       case "card-face":
         return this.http.get(`${this.apiUrl}/card-face/${fileName}`, { responseType: 'blob' });
       case "card-face-element-image":
         return this.http.get(`${this.apiUrl}/card-face-element-image/${fileName}`, { responseType: 'blob' });
-        default:
+      default:
         return this.http.get(`${this.apiUrl}/${fileName}`, { responseType: 'blob' });
     }
   }
@@ -29,41 +41,55 @@ export class FileUploadApiService {
   replaceFilePath$(fileName: string, type?: string): Observable<{ id: string | undefined; }> {
     switch (type) {
       case "card-face":
-        return this.http.put<{id: string}>(`${this.apiUrl}/card-face-image-path/${fileName}`, fileName);
+        return this.http.put<{ id: string }>(`${this.apiUrl}/card-face-image-path/${fileName}`, fileName);
       case "card-face-element-image":
-        return this.http.put<{id: string}>(`${this.apiUrl}/card-face-element-image-path/${fileName}`, fileName);
+        return this.http.put<{ id: string }>(`${this.apiUrl}/card-face-element-image-path/${fileName}`, fileName);
       default:
-         return this.http.put<{id: string}>(`${this.apiUrl}/card-face-element-image-path/${fileName}`, fileName);
+        return this.http.put<{ id: string }>(`${this.apiUrl}/card-face-element-image-path/${fileName}`, fileName);
     }
   }
 
-  // TODO: Do switch statement here
-  
+  replaceFilePaths$(fileNames: string[], type?: string): Observable<string[]> {
+    switch (type) {
+      case "card-face":
+        return this.http.put<string[]>(`${this.apiUrl}/card-face-image-path/batch`, fileNames);
+      default:
+        return this.http.put<string[]>(`${this.apiUrl}`, fileNames/*, {headers}*/);
+    }
+  }
+
   uploadFile$(formData: FormData, type?: string): Observable<{ id: string }> {
     let headers = new HttpHeaders().set('Content-Type', 'multipart/form-data');
-    
-    switch (type)
-    {
+
+    switch (type) {
       case "card-face":
-        return this.http.post<{id: string}>(`${this.apiUrl}/card-face`, formData);
+        return this.http.post<{ id: string }>(`${this.apiUrl}/card-face`, formData);
       case "card-face-element-image":
-        return this.http.post<{id: string}>(`${this.apiUrl}/card-face-element-image`, formData);
+        return this.http.post<{ id: string }>(`${this.apiUrl}/card-face-element-image`, formData);
       default:
-        return this.http.post<{id: string}>(`${this.apiUrl}`, formData/*, {headers}*/);
+        return this.http.post<{ id: string }>(`${this.apiUrl}`, formData/*, {headers}*/);
+    }
+  }
+
+  uploadFiles$(formData: FormData, type?: string): Observable<string[]> {
+    switch (type) {
+      case "card-face":
+        return this.http.post<string[]>(`${this.apiUrl}/card-face/lods`, formData);
+      default:
+        return this.http.post<string[]>(`${this.apiUrl}`, formData/*, {headers}*/);
     }
   }
 
   replaceFile(formData: FormData, fileName: string, type?: string): Observable<{
     id: string;
   }> {
-    switch (type)
-    {
+    switch (type) {
       case "card-face":
-        return this.http.put<{id: string}>(`${this.apiUrl}/card-face/${fileName}`, formData);
+        return this.http.put<{ id: string }>(`${this.apiUrl}/card-face/${fileName}`, formData);
       case "card-face-element-image":
-        return this.http.put<{id: string}>(`${this.apiUrl}/card-face-element-image/${fileName}`, formData);
+        return this.http.put<{ id: string }>(`${this.apiUrl}/card-face-element-image/${fileName}`, formData);
       default:
-        return this.http.put<{id: string}>(`${this.apiUrl}`, formData/*, {headers}*/);
+        return this.http.put<{ id: string }>(`${this.apiUrl}`, formData/*, {headers}*/);
     }
   }
 
