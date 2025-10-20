e9c3774ca5625908d494fd34b03e83783bd85340
dewolfe <>
2025-10-20 16:10:05 -0700

    fix: dynamically resized canvas

--- frontend/src/app/utils/services/atlas-export/atlas-export.service.ts
+++ frontend/src/app/utils/services/atlas-export/atlas-export.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { areDimensionsHigherThanZero, clamp, Coordinates, Dimensions, download, getMidpoint, normalize } from '../../utils';
+import { v4 as uuidv4 } from 'uuid';
+import { areDimensionsHigherThanZero, clamp, Coordinates, Dimensions, download, normalize } from '../../utils';
 
 export function guillotine(images: HTMLImageElement[], margin: number = 0.5): {
   imagePiecesWithCoordinates: Map<Coordinates, HTMLImageElement>,
@@ -19,6 +20,7 @@ export function guillotine(images: HTMLImageElement[], margin: number = 0.5): {
     isPiece: boolean = false;
 
     static nodes: Node[] = [];
+    static root: Node;
 
     // NOTE: We want to keep it 0-1 https://www.cyanilux.com/tutorials/sprite-local-uv/
     coordinates: Coordinates = {
@@ -27,8 +29,8 @@ export function guillotine(images: HTMLImageElement[], margin: number = 0.5): {
     };
 
     // CHECKME: Do we also add coordinates here?
-    constructor(id: string, dimensions?: Dimensions, parent?: Node | null, left?: Node | null, right?: Node | null) {
-      this.id = id;
+    constructor(id?: string, dimensions?: Dimensions, parent?: Node | null, left?: Node | null, right?: Node | null) {
+      this.id = id ?? uuidv4();
 
       this.dimensions = dimensions ?? {
         width: 0,
@@ -38,7 +40,7 @@ export function guillotine(images: HTMLImageElement[], margin: number = 0.5): {
       this.parent = parent ?? null;
       this.left = left ?? null;
       this.right = right ?? null;
-    
+
       Node.nodes.push(this);
     }
   }
@@ -52,12 +54,12 @@ export function guillotine(images: HTMLImageElement[], margin: number = 0.5): {
     node.right = null;
   }
 
-  function cut(id: string, parent: Node, side: 'w' | 'h', offset: number): 'w' | 'h' | undefined {
+  function cut(parent: Node, side: 'w' | 'h', offset: number): 'w' | 'h' | undefined {
     let centre: Coordinates = parent.coordinates;
 
     if (side == 'w' && offset <= parent.dimensions.width) {
-      parent.left = new Node(id, { width: offset, height: parent.dimensions.height }, parent);
-      parent.right = new Node(id, { width: parent.dimensions.width - offset, height: parent.dimensions.height }, parent);
+      parent.left = new Node(uuidv4(), { width: offset, height: parent.dimensions.height }, parent);
+      parent.right = new Node(uuidv4(), { width: parent.dimensions.width - offset, height: parent.dimensions.height }, parent);
 
       /*
       this.left.draw( { x: o.x - this.w/2 + this.left.w/2, y: o.y })
@@ -75,13 +77,13 @@ export function guillotine(images: HTMLImageElement[], margin: number = 0.5): {
     }
 
     if (side == 'h' && offset <= parent.dimensions.height) {
-      parent.left = new Node(id, { width: parent.dimensions.width, height: parent.dimensions.height - offset }, parent);
+      parent.left = new Node(uuidv4(), { width: parent.dimensions.width, height: parent.dimensions.height - offset }, parent);
       /*
       this.left.draw( { x: o.x, y: o.y - this.h/2 + this.right.h + this.left.h/2 })
       this.right.draw({ x: o.x, y: o.y - this.h/2 + this.right.h/2 })
       */
 
-      parent.right = new Node(id, { width: parent.dimensions.width, height: offset }, parent);
+      parent.right = new Node(uuidv4(), { width: parent.dimensions.width, height: offset }, parent);
 
       let halfParentHeight: number = parent.dimensions.height / 2;
       let halfLeftHeight: number = parent.left.dimensions.height / 2;
@@ -99,38 +101,113 @@ export function guillotine(images: HTMLImageElement[], margin: number = 0.5): {
 
   // NOTE: Attempts to place piece in existing node
   function canPush(piece: Node, margin: number): boolean {
-    for (let node of Node.nodes) {
-      // For expansion of the canvas if necessary
-      if (node.dimensions.width < piece.dimensions.width || node.dimensions.height < piece.dimensions.height) {
-        /*node.dimensions = {
-          width: (node.dimensions.width + piece.dimensions.width)/margin,
-          height: (node.dimensions.height + piece.dimensions.height)/margin
-        }*/
+    function splitNode(piece: Node, node: Node): boolean {
+      cut(node, 'w', piece.dimensions.width/* / margin*/);
 
-        console.warn(`Node: ${JSON.stringify(node.dimensions, null, 2)} is smaller than piece: ${JSON.stringify(piece.dimensions, null, 2)}`);
-        continue;
-      }
+      if (!node.left) throw new Error("Guillotine - node.left: When you cut a node, there should be a left and right");
 
-      if (!node.isPiece && !isCut(node)) {
-        // TODO: Potentially have user choice in how to cut? 
-        cut(piece.id, node, 'w', piece.dimensions.width/* / margin*/);
+      cut(node.left, 'h', piece.dimensions.height/* / margin*/);
 
-        if (!node.left) throw new Error("Guillotine - node.left: When you cut a node, there should be a left and right");
+      if (!node.left.right) throw new Error("Guillotine - node.left.right: When you cut a node, there should be a left and right");
 
-        cut(piece.id, node.left, 'h', piece.dimensions.height/* / margin*/);
-
-        if (!node.left.right) throw new Error("Guillotine - node.left.right: When you cut a node, there should be a left and right");
-
-        node.left.right.isPiece = true;
-        return true;
-      }
+      return true;
     }
 
-    return false;
+    function getPiece(piece: Node, node: Node): Node {
+      if (!splitNode(piece, node)) throw new Error("Could not split node: When you cut a node, there should be a left and right");
+
+      if (!node || !node.left || !node.left.right) throw new Error("Guillotine - node.left.right: When you cut a node, there should be a left and right");
+
+      node.left.right.isPiece = true;
+      return node.left.right;
+    }
+
+    // Essentially we need to find a fit node to place our piece inside
+    function findFitNode(node: Node, piece: Node): Node | null {
+      // So there's three possibilities: the node already fits the piece and isn't a piece itself
+      if (!node.isPiece && !isCut(node) && node.dimensions.width >= piece.dimensions.width && node.dimensions.height >= piece.dimensions.height) return node;
+
+      // The node has a left and or right node next to it that matches the first condition
+      if (isCut(node)) {
+        if (!node.left || !node.right) throw new Error("Guillotine: When you cut a node, there should be a left and right");
+
+        let fit: Node | null = findFitNode(node.left, piece);
+
+        if (fit) return fit;
+
+        return findFitNode(node.right, piece);
+      }
+
+      // Or there's no fit node
+      return null;
+    }
+
+    // So we want to find out whether the root node's already big enough, essentially if it isn't then we do stuff from there
+    let node: Node | null = findFitNode(Node.root, piece);
+    if (node) { // TODO: Potentially have user choice in how to cut? 
+      let leaf: Node = getPiece(piece, node);
+      leaf.id = piece.id; // We need to do this because we grab the specific pieces by their shared IDs
+
+      return true;
+    }
+
+
+    // This is to check whether the canvas needs to be expanded, which is the size of our root
+    // ASSUMPTION: Root's initial dimension will always be based on biggest item's dimensions
+    // We check if there's a difference because we need to see if there's an overlap, x >=0 means there is an overlap
+    let difference: Dimensions = {
+      width: Node.root.dimensions.width - piece.dimensions.width,
+      height: Node.root.dimensions.height - piece.dimensions.height
+    }
+
+    // BASE CASE: If there's no difference in dimension size, there's no need for the root to grow
+    if (difference.width < 0 && difference.height < 0) return false;
+
+    let resizedRootDimensions: Dimensions = {
+      width: (difference.width >= 0) ? Node.root.dimensions.width + piece.dimensions.width / margin : Node.root.dimensions.width,
+      height: (difference.height >= 0) ? Node.root.dimensions.height + piece.dimensions.height / margin : Node.root.dimensions.height
+    }
+
+    let resizedRootRightDimensions: Dimensions = {
+      width: (difference.width >= 0) ? piece.dimensions.width / margin : Node.root.dimensions.width,
+      height: (difference.height >= 0) ? piece.dimensions.height / margin : Node.root.dimensions.height
+    }
+
+    let resizedRoot: Node = new Node(uuidv4(), resizedRootDimensions, null, Node.root);
+    resizedRoot.coordinates = getCentre(resizedRootDimensions);
+
+    resizedRoot.right = new Node(uuidv4(), resizedRootRightDimensions, resizedRoot);
+    resizedRoot.right.coordinates = getCentre(resizedRootRightDimensions);
+
+    Node.root.parent = resizedRoot;
+    Node.root = resizedRoot;
+
+    // So we want to replace the root with a new root so we don't need to reposition our pieces
+    return canPush(piece, margin);
   }
 
   function shouldDraw(node: Node): boolean {
     return (!isCut(node) && areDimensionsHigherThanZero(node.dimensions) && node.isPiece);
+  }
+
+  // So this is to make sure coordinate alignments is correct, we start from the centre of the canvas, not top left
+  function getCentre(dimensions: Dimensions): Coordinates {
+    return {
+      x: dimensions.width / 2,
+      y: dimensions.height / 2
+    }
+  }
+
+  // For use with image nodes because the problem is position's relative to parent
+  function getAbsoluteCoordinates(node: Node): Coordinates {
+    if (!node.parent) return node.coordinates;
+
+    let parent: Coordinates = getAbsoluteCoordinates(node.parent);
+    // Move right, move down
+    return {
+      x: parent.x + node.coordinates.x,
+      y: parent.y + node.coordinates.y
+    }
   }
 
   margin = clamp(normalize(margin, 0.5, 1), 0.5, 1);
@@ -151,15 +228,13 @@ export function guillotine(images: HTMLImageElement[], margin: number = 0.5): {
     height: Math.max(...pieces.map(node => node.dimensions.height)) / margin
   }
 
-  let root: Node = new Node('root', canvasDimensions);
-  root.coordinates = { x: canvasDimensions.width / 2, y: canvasDimensions.height / 2 };
+  Node.root = new Node(`${uuidv4()}`, canvasDimensions);
+  Node.root.coordinates = getCentre(canvasDimensions);
 
   // https://github.com/mariowise/2d-guillotine-cutter/blob/master/js/tree.js
   pieces.forEach((piece: Node) => {
     canPush(piece, margin);
   });
-
-  // If should draw, then add coordinates to map and HTML Image Element based on ID
 
   let imagePiecesWithCoordinates: Map<Coordinates, HTMLImageElement> = new Map();
 
@@ -176,7 +251,7 @@ export function guillotine(images: HTMLImageElement[], margin: number = 0.5): {
     canvasDimensions: {
       width: Math.max(...Node.nodes.map(node => /*node.coordinates.x + */node.dimensions.width))/* / margin*/,
       height: Math.max(...Node.nodes.map(node => /*node.coordinates.y + */node.dimensions.height))/* / margin*/
-    }
+    },
   }
 }
 
@@ -185,13 +260,6 @@ export function guillotine(images: HTMLImageElement[], margin: number = 0.5): {
 })
 export class AtlasExportService {
   constructor() { }
-
-  // FUNCTION: We grab all files of that type passing in the fileUploadApiService and the file names
-
-  // The atlas should range from 0 - 1.
-  // Get the dimensions of the images
-  // Get the size of the padding based on that: 80/100 = x/y
-  // Then determine the size of the canvas
 
   atlasExport(images: HTMLImageElement[], margin: number = 0.5, fileName: string = 'atlas'): void {
     let final: {
@@ -206,8 +274,8 @@ export class AtlasExportService {
     canvas.height = final.canvasDimensions.height;
 
     let ctx: CanvasRenderingContext2D | null = canvas.getContext("2d");
-    final.imagePiecesWithCoordinates.forEach((value: HTMLImageElement, key: Coordinates, map: Map<Coordinates, HTMLImageElement>) => {
-      if (ctx && value.complete) ctx.drawImage(value, key.x /* + (value.width/2)*/, key.y /* + (value.height/2)*/); // CHECKME: The keys are 0, 0, so would we need to make sure it's shifted because the root node starts smack dab in the middle of the canvas?
+    final.imagePiecesWithCoordinates.forEach((value: HTMLImageElement, key: Coordinates) => {
+      if (ctx && value.complete) ctx.drawImage(value, (key.x  + (canvas.width / 2) - value.width)/2, (key.y + (canvas.height/2) - value.height)/2); // CHECKME: The keys are 0, 0, so would we need to make sure it's shifted because the root node starts smack dab in the middle of the canvas?
     });
 
     console.log('Atlas export canvas size', canvas.width, canvas.height);
@@ -218,68 +286,5 @@ export class AtlasExportService {
       download(fileName, 'png', URL.createObjectURL(blob));
     });
   }
-
-  /*
-  FUNCTION:  Pass in the parameters, 
-    Question is, do we want to pass in the list of blobs, or the list of images
-    Max amount of elements per row there are - https://www.cyanilux.com/tutorials/sprite-local-uv/
-    Margin as a number
-
-    The atlas should range from 0 - 1.
-  
-    Canvas has alpha so no background images
-    1 dimensional array: so we pass in the images =  [0, 1, 2, 3, 4, 5] == { {0, 1, 2}, {3, 4, 5} }
-
-    To get a 1 dimensional array from a 2d matrix: it would be z = x * y. So to work backwards, we would get: x = z/y.
-    // z: 1D array
-    // y: Max amount of elements in one row - which determines the size of the column
-    // x: How many rows there actually are
-
-    // Index: y * rows + x;
-
-    canvas width = max width out of all three widths
-    canvas height = max height out of all elements in row (a) + max height out of all elements in row (b) + max height out of all elements in row (c) - keep in mind padding
-
-    // https://cplusplus.com/forum/general/282765/
-    // number of columns * desired row + desired column
-
-    let potentialCanvasWidths: number[];
-    let totalCanvasHeight: number;
-    
-    for (x: int = 0; x < rows; x++) {  //rows
-      let potentialCanvasWidth: number = 0; // Add all the widths together to then add to potentialCanvasWidths
-
-      let currentRowHeight: number = 0;
-
-      for (y: int = 0; y < columns; y++) {   //columns
-        index = y * rows + x;
-        image = arr_of_images[index];
-
-        Get the dimensions of the image
-        potentialCanvasWidth += image.dimensions.width/normalize(margin, 0, 1);
-
-        let height: number = image.dimensions.height/normalize(margin, 0, 1);
-        
-        if (currentRowHeight < height) currentRowHeight = height;
-      }
-
-      potentialCanvasWidths.push(potentialCanvasWidth);
-      potentialCanvasWidth = 0;
-
-      totalCanvasHeight += currentRowHeight;
-    }
-
-    let totalCanvasWidth: number = Math.max(potentialCanvasWidths);
-
-    let canvas = document.getElementById("my-house");
-    canvas.width = totalCanvasWidth;
-    canvas.height = totalCanvasHeight;
-    
-    let ctx = canvas.getContext("2d");
-
-    ctx.drawImage(image, x, y);
-
-    Normalise after everything - maybe?
-  */
 }
 
